33baa1167ac71a7d03e2f4dfca04e6a2bde435dd
Kumaravel-Ayyappan <kumaravel.ayyappan@gmail.com>
2020-08-26 17:14:01 +0530

    Initial Commit

---
+++ My_Solutions/plot4.R
@@ -0,0 +1,12 @@
+## Creating Plot4
+
+library(dplyr)
+
+# Getting data
+raw <- read.table("power.txt", header = TRUE, sep = ";", na.strings ="?")
+
+# Changing type for date column
+raw$Date <- as.Date(raw$Date, format = "%d/%m/%Y")
+
+# Subset required data
+reqdata <- subset(raw, Date == "2007-02-01" | Date == "2007-02-02")
