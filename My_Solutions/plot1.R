89ebc3e8c083158917e9179fb7f36813e59b59dd
Kumaravel-Ayyappan <kumaravel.ayyappan@gmail.com>
2020-08-26 10:35:25 +0530

    Intial Commit

---
+++ My_Solutions/plot1.R
@@ -0,0 +1,19 @@
+## Creating Plot1
+
+# Getting data
+raw <- read.table("power.txt", header = TRUE, sep = ";", na.strings ="?")
+
+# Changing type for date column
+raw$Date <- as.Date(raw$Date, format = "%d/%m/%Y")
+
+# subsetting required data
+reqdata <- subset(raw, Date == "2007-02-01" | Date == "2007-02-02")
+
+## Plot 1
+X11(width = 6.7, height = 6.7)
+hist(reqdata$Global_active_power, col = "red",
+     xlab = "Global Active Power (kilowatts)", ylab = "frequency",
+     main =  "Global Active Power",
+     bg = "white")
+dev.copy(png, "plot1.png")
+dev.off()
