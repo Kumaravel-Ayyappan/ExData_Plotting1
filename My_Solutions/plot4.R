6a755038b52b0086311cd46620b77f28d9785a43
Kumaravel-Ayyappan <kumaravel.ayyappan@gmail.com>
2020-08-26 17:35:03 +0530

    Finished Code

--- My_Solutions/plot4.R
+++ My_Solutions/plot4.R
@@ -20,19 +20,26 @@ par(mfcol = c(2, 2))
 
 #Plot1
 with(reqdata, plot(DT, Global_active_power, type = "l", xlab = "",
-                   ylab = "Global Active Power (kilowatts)"))
+                   ylab = "Global Active Power"))
 
 #Plot2
-with(reqdata, plot(DT, Sub_metering_1, type = "l", xlab ="", ylab = "Energy Sub Metering"))
+with(reqdata, plot(DT, Sub_metering_1, type = "l", xlab ="", 
+                   ylab = "Energy Sub Metering"))
 with(reqdata, lines(DT, Sub_metering_2, col = "red"))
 with(reqdata, lines(DT, Sub_metering_3, col = "blue"))
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty = c(1, 1, 1), col = c("black", "red", "blue"))
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1, 1, 1), col = c("black", "red", "blue"), box.lwd = 0)
 
 #Plot3
-with(reqdata, plot(DT, Voltage, xlab = "datetime", ylab = "Voltage", type = "l"))
+with(reqdata, plot(DT, Voltage, xlab = "datetime", 
+                   ylab = "Voltage", type = "l"))
 
 #plot4
 with(reqdata, plot(DT, Global_reactive_power, type = "l", xlab = "datetime"))
+
+#Saving as PNG
+dev.copy(png, file = "plot4.png")
+dev.off()
 
 
